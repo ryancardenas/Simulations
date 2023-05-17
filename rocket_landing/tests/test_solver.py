5891ac376e58a2476ebabd28e1c643fa88d608fa
Ryan Cardenas <31240617+ryancardenas@users.noreply.github.com>
2023-05-16 23:40:04 -0700

    Test/pass create_r_vector()

--- rocket_landing/tests/test_solver.py
+++ rocket_landing/tests/test_solver.py
@@ -16,6 +16,28 @@ import numpy as np
 import simulations.rocket_landing.solver as solver
 
 
+@pytest.mark.parametrize("r_planet, r0", [(1e6, 2.3e6), (3.2e8, 6.1e8), (5.0e7, 3e8)])
+@pytest.mark.parametrize("dr", np.arange(1e3, 1e6, 1e5))
+class Test_create_r_vector:
+    """Tests that create_r_vector() meets the following expectations:
+    - Result[0] equals r_planet.
+    - Result[-1] is between r0 and (r0 + dr).
+    - Elements are equally spaced by dr.
+    """
+
+    def test_result_first_element_equals_r_planet(self, r_planet, r0, dr):
+        result = solver.create_r_vector(r_planet=r_planet, r0=r0, dr=dr)
+        assert result[0] == r_planet
+
+    def test_result_last_element_within_expected_bounds(self, r_planet, r0, dr):
+        result = solver.create_r_vector(r_planet=r_planet, r0=r0, dr=dr)
+        assert r0 <= result[-1] <= (r0 + dr)
+
+    def test_elements_equally_spaced_by_dr(self, r_planet, r0, dr):
+        result = solver.create_r_vector(r_planet=r_planet, r0=r0, dr=dr)
+        assert np.allclose(np.diff(result), dr)
+
+
 class Test_get_index:
     """Tests that get_index() meets the following expectations:
     - Returns exact index for values in x_vector.
